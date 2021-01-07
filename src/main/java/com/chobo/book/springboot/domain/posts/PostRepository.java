6d66eddc17bdea2c83f4a238fe374f83db3dbe55
KMKLIFE <mk022713@gmail.com>
2021-01-08 01:02:34 +0900

    JPA H2 추가 및 SpringBoot 환경설정

---
+++ src/main/java/com/chobo/book/springboot/domain/posts/PostRepository.java
@@ -0,0 +1,7 @@
+package com.chobo.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ src/main/java/com/chobo/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.chobo.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id // Posts 테이블의 PK 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // GenerationType.IDENTITY을 해주어야 auto increment가 생성된다.
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
